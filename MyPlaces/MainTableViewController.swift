94e5250b81141dcc1cb3bdef13d93a51035572ef
mrDuderino <36927431+mrDuderino@users.noreply.github.com>
2022-05-08 22:51:08 +0300

    Use leadingSwipeActionsConfigurationForRowAt instead of deprecated editActionsForRowAt

--- MyPlaces/MainTableViewController.swift
+++ MyPlaces/MainTableViewController.swift
@@ -15,17 +15,17 @@ class MainTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         places = realm.objects(Place.self)
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return places.isEmpty ? 0 : places.count
     }
-
-
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         let place = places[indexPath.row]
@@ -42,24 +42,34 @@ class MainTableViewController: UITableViewController {
     
     // MARK: Table view delegate
     
-    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let place = places[indexPath.row]
-        let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { _, _ in
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _, _, _ in
             StorageManager.deleteObject(place: place)
             tableView.deleteRows(at: [indexPath], with: .automatic)
         }
-        return [deleteAction]
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
+        return configuration
     }
     
+    //    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+    //        let place = places[indexPath.row]
+    //        let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { _, _ in
+    //            StorageManager.deleteObject(place: place)
+    //            tableView.deleteRows(at: [indexPath], with: .automatic)
+    //        }
+    //        return [deleteAction]
+    //    }
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
         guard let newPlaceVC = segue.source as? NewPlaceViewController else {return}
